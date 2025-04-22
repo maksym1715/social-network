a902950208bd1a6ff90d15bf98cc1a289036ec77
Lenovo <Lenovo@10.100.102.14>
2025-04-22 23:59:36 +0300

    docker-composer mongo

--- social-network/src/main/java/project/social_network/posts/Comment.java
+++ social-network/src/main/java/project/social_network/posts/Comment.java
@@ -1,5 +1,45 @@
 package project.social_network.posts;
 
+import java.time.LocalDateTime;
+
 public class Comment {
 
+    private String authorId;
+    private String content;
+    private LocalDateTime createdAt;
+
+    // Constructors
+    public Comment() {
+    }
+
+    public Comment(String authorId, String content, LocalDateTime createdAt) {
+        this.authorId = authorId;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    // Getters and Setters
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
